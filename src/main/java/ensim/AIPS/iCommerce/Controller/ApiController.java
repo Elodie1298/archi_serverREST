54dcde786a5d4737f0c6b0e2ef9f26329231a434
Elodie Morin <Elodie.morin1298@gmail.com>
2019-03-11 12:22:11 +0100

    Controller done -> go to test

--- src/main/java/ensim/AIPS/iCommerce/Controller/ApiController.java
+++ src/main/java/ensim/AIPS/iCommerce/Controller/ApiController.java
@@ -1,50 +1,54 @@
 package ensim.AIPS.iCommerce.Controller;
 
 import ensim.AIPS.iCommerce.Model.Product;
+import ensim.AIPS.iCommerce.Model.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
-    // Linker au controller firebase
+    @Autowired
+    ProductRepository productRepository;
+
     @GetMapping(value="/product")
     public List<Product> getProductList() {
-        return new ArrayList<Product>();
+        return (List<Product>) productRepository.findAll();
     }
 
-    // Verifier type id
-    // Linker au controller firebase
     @GetMapping(value="/product/{id}")
     public Product getProduct(@PathVariable("id") long id) {
-        return new Product(0, null, null, 0);
+        return productRepository.findById(id).get();
     }
 
-    // Verifier type id
-    // Linker au controller firebase
+    // Vérifier la modification
     @PutMapping(value="/product/{id}")
     public Product updateProduct(@PathVariable("id") long id,
                                  @RequestBody Product product) {
-        System.out.println("product.getName() = " + product.getName());
-        return new Product(0, null, null, 0);
+        if (productRepository.findById(id).get().getId() == id) {
+            productRepository.findById(id).get().setName(product.getName());
+            productRepository.findById(id).get().setDescription(product.getDescription());
+            productRepository.findById(id).get().setStock(product.getStock());
+            return getProduct(id);
+        }
+        else {
+            return null;
+        }
     }
 
-    // Verifier type id
-    // Linker au controller firebase
+    // Vérifier le changement
     @DeleteMapping(value="/product/{id}")
-    public Product deleteProduct(@PathVariable("id") long id) {
-        return new Product(0, null, null, 0);
+    public void deleteProduct(@PathVariable("id") long id) {
+        productRepository.deleteById(id);
     }
 
-    // Verifier type id
-    // Linker au controller firebase
+    // Vérifier le changement
     // Linker à l'authentification
     @PostMapping(value="/product")
-    public Product addProduct(@RequestBody Product product) {
-        System.out.println("product = " + product.getName());
-        return new Product(0, null, null, 0);
+    public void addProduct(@RequestBody Product product) {
+        productRepository.save(product);
     }
 }
