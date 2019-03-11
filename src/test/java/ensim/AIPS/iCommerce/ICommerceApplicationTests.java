8f2964ac75b481b31cec9fb0c4a6dd0949a9f1fa
Victor Morgant <victor.morgant@gmail.com>
2019-03-11 11:23:35 +0100

    Spring configuration with SpringBoot

---
+++ src/test/java/ensim/AIPS/iCommerce/ICommerceApplicationTests.java
@@ -0,0 +1,16 @@
+package ensim.AIPS.iCommerce;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ICommerceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
